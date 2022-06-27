8f0a7d2af67e8658c0d628e2b2555a952514ad60
israel.souza <israel.souza@summit-bra.com>
2022-06-27 11:53:30 -0300

    keycloak

--- cadastro/src/main/java/io/codekaffee/ifood/cadastro/resources/RestaurantResource.java
+++ cadastro/src/main/java/io/codekaffee/ifood/cadastro/resources/RestaurantResource.java
@@ -2,6 +2,7 @@ package io.codekaffee.ifood.cadastro.resources;
 
 import java.util.List;
 
+import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
@@ -18,11 +19,16 @@ import javax.ws.rs.core.Response.Status;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponseSchema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlow;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlows;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.eclipse.microprofile.openapi.annotations.tags.Tags;
 
@@ -36,6 +42,13 @@ import io.codekaffee.ifood.cadastro.services.RestaurantService;
 @Path(value = "/restaurantes")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
+@RolesAllowed("proprietario")
+@SecurityScheme(
+    securitySchemeName = "ifood-oauth", 
+    type = SecuritySchemeType.OAUTH2, 
+    flows = @OAuthFlows(password = @OAuthFlow(tokenUrl = "http://localhost:8180/auth/realms/ifood/protocol/openid-connect/token") )
+)
+@SecurityRequirement(name = "ifood-oauth", scopes = {"proprietario"})
 public class RestaurantResource {
 
     private final RestaurantService restaurantService;
